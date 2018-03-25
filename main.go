53b1dd2176780b6500a99abf0ab3a85471f35677
Alec Cunningham <aleccunningham96@gmail.com>
2018-03-24 21:11:11 -0500

    Update main.go

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 		"google.golang.org/grpc/metadata"
 
 		"golang.org/x/crypto/acme/autocert"
+		"github.com/eapache/channels"
 		"github.com/cncd/logging"
 		"github.com/marjoram/pipelinne/pipeline/rpc/proto"
 		"github.com/marjoram/pubsub"
@@ -23,7 +24,24 @@ import (
 
 func main() {
 		ctx := context.Background()
-		
+		// SharedBuffers satisfy the Buffer interface, and also permits multiple SimpleChannel
+		// instances to share a single buffer, while maintaining individual queues
+		buf := channels.NewSharedBuffer(3)
+		// Pipe connects the input channel to the output channel so they
+		// behave as if they were a single channel
+		channels.Pipe(ch1, ch2)
+		// ch1 takes the input
+		go func() {
+			for i := 0; i < 20; i++ {
+				ch1.In() <- i
+			}
+			ch1.Close()
+		}()
+	
+		// ch2 displays the output
+		// for _ = range ch2.Out() { /* */ }
+		buf.Close()
+	
 		var g errgroup.Group
 		
 		// start grpc server
