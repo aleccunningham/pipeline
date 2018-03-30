5d189bc6f7d92754d419fa0e4baa14438ef5f8fc
aleccunningham <aleccunningham96@gmail.com>
2018-03-30 00:25:01 -0500

    refactor

--- version/version.go
+++ version/version.go
@@ -1,17 +1,49 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package version
 
-import "fmt"
-
-var (
-	// major is for an API incompatible changes
-	major = 1
-	// minor is for functionality in a backwards-compatible manner
-	minor = 0
-	// patch is for backwards-compatible bug fixes
-	patch = 0
+import (
+	"fmt"
+	"runtime"
 )
 
-// String returns the supporeted specification versions in string format.
-func String() string {
-	return fmt.Sprintf("%d.%d.%d", major, minor, patch)
+var version, gitCommit, gitTreeState, buildDate string
+
+type Info struct {
+	Version      string
+	GitVersion   string
+	GitCommit    string
+	GitTreeState string
+	BuildDate    string
+	GoVersion    string
+	Compiler     string
+	Platform     string
+}
+
+// Get returns the version and buildtime information about the binary
+func Get() *Info {
+	// These variables typically come from -ldflags settings to `go build`
+	return &Info{
+		Version:      version,
+		GitCommit:    gitCommit,
+		GitTreeState: gitTreeState,
+		BuildDate:    buildDate,
+		GoVersion:    runtime.Version(),
+		Compiler:     runtime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	}
 }
